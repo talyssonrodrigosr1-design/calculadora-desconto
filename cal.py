2acd0865da7bc587c5863da20c60de25babbc59f
talyssonrodrigosr1-design <talyssonrodrigosr1@gmail.com>
2026-09-15 20:04:37 -0300

    Create cal.py

---
+++ cal.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.title("Calculadora de Descontos")
+preco_original=st.number_input("Digite o Valor do Serviço")
+porgetagem=st.number_input("Digite a porgetagem do produto")
+if st.button("Calcular"):
+   valor_desconto= preco_original*(porgetagem/100)
+   valor_final=preco_original-valor_desconto
+    st.write(f'Meu amor o Valor pra cobrar do cliente é:{valor_final:.2f}')
